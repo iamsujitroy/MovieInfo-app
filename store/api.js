4a87f35d6183b36d4b64d9f8c4bc944af6d968b0
Sujit Roy <sujitroy78148@gmail.com>
2022-09-15 19:21:19 +0530

    new changes

--- store/api.js
+++ store/api.js
@@ -1,3 +1,5 @@
-export default {
+const apis = {
+    POPULAR_MOVIES: "movie/popular?api_key=d7c0bc71fa728e20535a53dc46a2e97a&language=en-US&page=",
     
 }
+export default apis;
--- store/index.js
+++ store/index.js
@@ -1,6 +1,3 @@
-export const state = () => ({
-  API_KEY: "d7c0bc71fa728e20535a53dc46a2e97a",
-});
+import { apis } from "./api";
 
-
-// https://api.themoviedb.org/3/  configuration/jobs  ?api_key=d7c0bc71fa728e20535a53dc46a2e97a
+export const state = () => ({});
